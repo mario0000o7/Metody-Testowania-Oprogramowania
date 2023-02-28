7eca49716215bae4fb735e5cd443b4837617432d
jancys <duchola1@gmail.com>
2023-02-28 18:39:54 +0100

    Dodanie swapcase

--- lab1/main.py
+++ lab1/main.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
+
 import sys
 
-def my_printf(string_param,param):
-    a=string_param.find("#k")
-    if a==-1:
-        print(string_param,end="")
-    else:
-        print(string_param[0:a],end="")
-        print(param,end="")
-        print(string_param[a+2:len(string_param)],end="")
-
-
-# my_printf("Hello #k world","param")
+def my_printf(format_string,param):
+    #print(format_string)
+    shouldDo=True
+    for idx in range(0,len(format_string)):
+        if shouldDo:
+            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+                print(param.swapcase(),end="")
+                shouldDo=False
+            else:
+                print(format_string[idx].swapcase(),end="")
+        else:
+            shouldDo=True
+    print("")
 
 data=sys.stdin.readlines()
 
-
 for i in range(0,len(data),2):
     my_printf(data[i].rstrip(),data[i+1].rstrip())
-
-
-
-
