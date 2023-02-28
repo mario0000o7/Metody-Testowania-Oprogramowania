985541be993c9c3796fee8d0c631c4659c4144d5
jancys <duchola1@gmail.com>
2023-02-28 18:30:57 +0100

    Dodanie end='' przy print

--- lab1/main.py
+++ lab1/main.py
@@ -4,18 +4,20 @@ import sys
 def my_printf(string_param,param):
     a=string_param.find("#k")
     if a==-1:
-        print(string_param)
+        print(string_param,end="")
     else:
-        print(string_param[0:a])
-        print(param)
-        print(string_param[a+2:len(string_param)])
+        print(string_param[0:a],end="")
+        print(param,end="")
+        print(string_param[a+2:len(string_param)],end="")
 
 
-data=sys.stdin.readlines()
+my_printf("Hello #k world","param")
 
-
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+# data=sys.stdin.readlines()
+#
+#
+# for i in range(0,len(data),2):
+#     my_printf(data[i].rstrip(),data[i+1].rstrip())
 
 
 
